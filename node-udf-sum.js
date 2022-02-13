ae8798cff8c100f42b601f82c86c0edbbb8ae358
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2022-02-13 22:43:01 +0100

    Update node-udf-sum.js

--- node-udf-sum.js
+++ node-udf-sum.js
@@ -11,8 +11,8 @@ async function read(stream) {
 (async function() {  
    // Read full STDIN
    var input = await read(process.stdin);
-   // Remove the last /n and split by /t
-   const parsed = input.slice(0,-1).split('\t');
+   // Trim and Split by /t
+   const parsed = input.trim().split('\t');
    // Perform any logic
    var sum = parseInt(parsed[0]) + parseInt(parsed[1])
    // Print results back to stdout
